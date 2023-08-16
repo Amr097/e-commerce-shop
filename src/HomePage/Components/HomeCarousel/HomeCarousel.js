284636fb6aa6be6a5a142dcb4590e17e8f3d69e8
Amr <amruser97@gmail.com>
2023-08-16 09:46:28 +0300

    updated home swiper

--- src/HomePage/Components/HomeCarousel/HomeCarousel.js
+++ src/HomePage/Components/HomeCarousel/HomeCarousel.js
@@ -1,54 +1,95 @@
 "use client";
-import { React, useState } from "react";
+import React, { useState } from "react";
 import "./HomeCarousel.scss";
 import { products } from "@/data";
+import Image from "next/image";
 import { v4 as uuidv4 } from "uuid";
-import CarouselSlide from "./Components/CarouselSlide";
-import { validateAndSlide } from "./Functions/slideCarousel";
+import { FaArrowRight } from "react-icons/fa";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const HomeCarousel = () => {
-  const [index, setIndex] = useState(0);
   return (
-    <div className="carousel">
-      <div className="carousel__arrow">
-        <input
-          type="radio"
-          name="arrow"
-          className="carousel__arrow--left"
-          onClick={() => validateAndSlide("left", index, setIndex)}
-        ></input>
-        <input
-          name="arrow"
-          type="radio"
-          className="carousel__arrow--right"
-          onClick={() => validateAndSlide("right", index, setIndex)}
-        ></input>
-      </div>
-      <ul className="carousel__list">
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={false}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
         {products.map((product) => (
-          <CarouselSlide
-            product={product}
-            products={products}
-            index={index}
-            setIndex={setIndex}
-            key={uuidv4()}
-          />
+          <SwiperSlide key={uuidv4()}>
+            <Image
+              className="swiper__image"
+              width={3000}
+              height={3000}
+              src={product.image}
+              alt={product.type + "" + product.category}
+            />
+            <h1 className="swiper__title heading-huge">
+              Level up your style with our summer collections
+            </h1>
+            <a className="swiper__btn btn-primary">
+              Shop now{" "}
+              <span className="btn__arrow">
+                {" "}
+                <FaArrowRight />
+              </span>
+            </a>
+          </SwiperSlide>
         ))}
-      </ul>
-      <div className="carousel__dots">
-        {products.map((dot, dotIndex) => (
-          <span
-            key={uuidv4()}
-            className={index === dotIndex ? "carousel__dots--active" : ""}
-            onClick={() => {
-              validateAndSlide("dot", index, setIndex, dotIndex);
-            }}
-          >
-            •
-          </span>
-        ))}
-      </div>
-    </div>
+      </Swiper>
+
+      {/* <div className="carousel">
+        <div className="carousel__arrow">
+          <input
+            type="radio"
+            name="arrow"
+            className="carousel__arrow--left"
+            onClick={() => validateAndSlide("left", index, setIndex)}
+          ></input>
+          <input
+            name="arrow"
+            type="radio"
+            className="carousel__arrow--right"
+            onClick={() => validateAndSlide("right", index, setIndex)}
+          ></input>
+        </div>
+        <ul className="carousel__list">
+          {products.map((product) => (
+            <CarouselSlide
+              product={product}
+              products={products}
+              index={index}
+              setIndex={setIndex}
+              key={uuidv4()}
+            />
+          ))}
+        </ul>
+        <div className="carousel__dots">
+          {products.map((dotIndex) => (
+            <span
+              key={uuidv4()}
+              className={index === dotIndex ? "carousel__dots--active" : ""}
+              onClick={() => {
+                validateAndSlide("dot", index, setIndex, dotIndex);
+              }}
+            >
+              •
+            </span>
+          ))}
+        </div>
+      </div> */}
+    </>
   );
 };
 
--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -6,7 +6,25 @@ import "./Home.scss";
 
 const HomePage = () => {
   return (
-    <section className="home">
+    <section
+      className="home"
+      onClick={(e) => {
+        const nextArrow = document.querySelector(".swiper-button-next");
+        const prevArrow = document.querySelector(".swiper-button-prev");
+
+        if (e.target === nextArrow) {
+          nextArrow.classList.add("arrow-right-animation");
+          setTimeout(() => {
+            nextArrow.classList.remove("arrow-right-animation");
+          }, 320);
+        } else if (e.target === prevArrow) {
+          prevArrow.classList.add("arrow-left-animation");
+          setTimeout(() => {
+            prevArrow.classList.remove("arrow-left-animation");
+          }, 320);
+        }
+      }}
+    >
       <HomeCarousel />
       <CurratedPicks />
       <FeaturedProducts />
